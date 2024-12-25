d22e781e039c4ca8dfeae8eeeeb2e1456d89f23a
arunk2493 <arunk249311@gmail.com>
2024-12-26 01:55:33 +0530

    Removed files

--- src/global-setup.ts
+++
@@ -1,9 +0,0 @@
-// import { LMStudioAPI } from '../src/utils/lm-studio-helper';
-
-// const lmStudioAPI = new LMStudioAPI('http://127.0.0.1:1234');
-
-// module.exports = async () => {
-//     const modelPath = process.env.LM_MODEL_PATH || '/Users/arunkumaraswamy/.cache/lm-studio/models/mlx-community/Llama-3.2-3B-Instruct-4bit';
-//     console.log(`Global Setup: Starting LM Studio model at ${modelPath}...`);
-//     await lmStudioAPI.startModel(modelPath);
-// };
